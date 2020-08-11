6a4df9b5dacdcea211287dfcc12e612108af502a
Matt Fava <matt.b.fava@gmail.com>
2020-08-11 17:06:13 -0400

    moved employeeData outside of the app.js file into a seperate file to allow for the addition of employees without accessing the app.js file

--- src/App.js
+++ src/App.js
@@ -3,28 +3,10 @@ import './App.css';
 import Header from './components/header';
 import Table from './components/table';
 import Footer from './components/footer';
+import employeeData from './employeeData';
 
 
-const employeeData = [
-  {
-      name: "Matt Huginbother",
-      title: "Accountant",
-      department: "Finance",
-      start_date: "07-12-2012"
-  },
-  {
-      name: "Jane Roe",
-      title: "Front End Developer",
-      department: "Development",
-      start_date: "05-31-2009"
-  },
-  {
-      name: "John Sage",
-      title: "Data Analyst",
-      department: "Data",
-      start_date: "10-23-2005"
-  }
-]
+
 
 
 function App() {
